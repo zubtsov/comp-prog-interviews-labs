662eb40de44f449732471c85ed077b56ba4eb97e
Ruslan Zubtsov <zubtsov.r@gmail.com>
2024-06-13 22:31:02 +0400

    - added lab for PCPP1

--- edube.org/python-advanced-1/python-core-syntax-lab-2.py
+++ edube.org/python-advanced-1/python-core-syntax-lab-2.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 class Duration:
     def __init__(self, hours=0, minutes=0, seconds=0):
         self._seconds = hours * 3600 + minutes * 60 + seconds
@@ -14,17 +17,21 @@ class Duration:
     def __str__(self):
         return f'{self._get_hours():02}:{self._get_minutes():02}:{self.get_seconds():02}'
 
-    def __add__(self, other: 'Duration') -> 'Duration':
-        if not isinstance(other, Duration):
+    def __add__(self, other: Union['Duration', int]) -> 'Duration':
+        if isinstance(other, Duration):
+            return Duration(seconds=self._seconds + other._seconds)
+        elif isinstance(other, int):
+            return Duration(seconds=self._seconds + other)
+        else:
             raise TypeError(type(other))
 
-        return Duration(seconds=self._seconds + other._seconds)
-
-    def __sub__(self, other: 'Duration') -> 'Duration':
-        if not isinstance(other, Duration):
+    def __sub__(self, other: Union['Duration', int]) -> 'Duration':
+        if isinstance(other, Duration):
+            return Duration(seconds=max(self._seconds - other._seconds, 0))
+        elif isinstance(other, int):
+            return Duration(seconds=max(self._seconds - other, 0))
+        else:
             raise TypeError(type(other))
-
-        return Duration(seconds=max(self._seconds - other._seconds, 0))
 
     def __mul__(self, other: int) -> 'Duration':
         if not isinstance(other, int):
@@ -42,3 +49,7 @@ if __name__ == '__main__':
     assert str(d1 + d2) == '23:44:12'
     assert str(d1 - d2) == '20:13:28'
     assert str(d1 * 2) == '43:57:40'
+
+    d3 = Duration(hours=21, minutes=58, seconds=50)
+    assert str(d3 + 62) == '21:59:52'
+    assert str(d3 - 62) == '21:57:48'
