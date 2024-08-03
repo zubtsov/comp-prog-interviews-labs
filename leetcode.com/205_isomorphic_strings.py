01350d11fc3011caac0d6b21adc33b79cf78409e
Ruslan Zubtsov <zubtsov.r@gmail.com>
2024-08-03 15:59:08 +0400

    - 205. Isomorphic Strings

--- leetcode.com/205_isomorphic_strings.py
+++ leetcode.com/205_isomorphic_strings.py
@@ -2,6 +2,22 @@
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        last_seen_s_index = [None] * 128
+        last_seen_t_index = [None] * 128
+
+        for i in range(len(s)):
+            si_ord = ord(s[i])
+            ti_ord = ord(t[i])
+
+            if last_seen_s_index[si_ord] != last_seen_t_index[ti_ord]:
+                return False
+
+            last_seen_s_index[si_ord] = i
+            last_seen_t_index[ti_ord] = i
+
+        return True
+
+    def isIsomorphic2(self, s: str, t: str) -> bool:
         s_to_t = [None] * 128
         t_to_s = [None] * 128
 
